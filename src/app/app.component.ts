bbdd22fa7e799a34be94804c212568c014b4df28
ShanmugamGitHub <151519340+ShanmugamGitHub@users.noreply.github.com>
2023-12-03 23:28:55 +0530

    1St trail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CRM-Project';
+  
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+  isMainNavOpen = true;
+  isSmallNavOpen = false;
+
+  constructor() {}
+
+  toggleMainNav(): void {
+    this.isMainNavOpen = !this.isMainNavOpen;
+    
+    // If the main nav is closed, automatically show the small nav
+    if (!this.isMainNavOpen) {
+      this.isSmallNavOpen = true;
+    } else {
+      // If the main nav is open, close the small nav
+      this.isSmallNavOpen = false;
+    }
+  }
+
+  toggleSmallNav(): void {
+    this.isSmallNavOpen = !this.isSmallNavOpen;
+  }
 }
